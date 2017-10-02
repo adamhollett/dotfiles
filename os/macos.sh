f6f124abfec3b12bfb09c1e2fdde8de2136a9037
Adam Hollett <mail@adamhollett.com>
2017-10-02 09:48:52 -0400

    Prepare for multi-platform

---
+++ os/macos.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Symlinks
+
+symlink_to_home() {
+  FILE=$1
+  # Only take action if the input file exists
+  if [ -f $FILE ]
+  then
+    # Symlink the file to the home directory
+    echo "Linking $(basename $FILE)"
+    ln -sf $FILE ~/$(basename $FILE)
+  fi
+}
+
+print_header 'create symlinks'
+
+## shell
+
+symlink_to_home ~/dotfiles/shell/.inputrc
+
+### bash
+
+symlink_to_home ~/dotfiles/shell/bash/.bash_profile
+symlink_to_home ~/dotfiles/shell/bash/.bashrc
+
+### zsh
+
+symlink_to_home ~/dotfiles/shell/zsh/.zprofile
+symlink_to_home ~/dotfiles/shell/zsh/.zshrc
+
+## git
+
+symlink_to_home ~/dotfiles/git/.gitconfig
+symlink_to_home ~/dotfiles/git/.gitignore_global
+
+## ruby
+
+symlink_to_home ~/dotfiles/ruby/.ruby-version
+symlink_to_home ~/dotfiles/ruby/.rubocop.yml
+
+## brew
+
+symlink_to_home ~/dotfiles/brew/Brewfile
+
+
+# Applications
+
+cd ~
+
+print_header 'install and update apps'
+
+brew bundle
--- setup.sh
+++ setup.sh
@@ -1,56 +1,25 @@
 #!/bin/sh
 
-# Symlinks
-
-symlink_to_home() {
-  FILE=$1
-  # Only take action if the input file exists
-  if [ -f $FILE ]
-  then
-    # Symlink the file to the home directory
-    echo "Linking $(basename $FILE)"
-    ln -sf $FILE ~/$(basename $FILE)
-  fi
+print_header() {
+  header=$1
+  echo ''
+  echo '›'
+  echo "› $header"
+  echo '›'
+  echo ''
 }
 
-echo ''
-echo '› create symlinks'
-echo ''
-
-## shell
-
-symlink_to_home ~/dotfiles/shell/.inputrc
-
-### bash
-
-symlink_to_home ~/dotfiles/shell/bash/.bash_profile
-symlink_to_home ~/dotfiles/shell/bash/.bashrc
-
-### zsh
-
-symlink_to_home ~/dotfiles/shell/zsh/.zprofile
-symlink_to_home ~/dotfiles/shell/zsh/.zshrc
-
-## git
-
-symlink_to_home ~/dotfiles/git/.gitconfig
-symlink_to_home ~/dotfiles/git/.gitignore_global
-
-## ruby
-
-symlink_to_home ~/dotfiles/ruby/.ruby-version
-symlink_to_home ~/dotfiles/ruby/.rubocop.yml
-
-## brew
-
-symlink_to_home ~/dotfiles/brew/Brewfile
-
-# Applications
-
-cd ~
-
-echo ''
-echo '› install and update apps'
-echo ''
-
-brew bundle
+# Determine which OS we are in and execute the corresponding script
+case $OSTYPE in
+  darwin*)
+    print_header 'operating system detected as macOS'
+    source os/macos.sh
+    ;;
+  msys)
+    print_header 'operating system detected as Windows'
+    source os/windows.sh
+    ;;
+  *)
+    exit
+    ;;
+esac
