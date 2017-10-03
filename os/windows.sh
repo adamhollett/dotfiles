d19b38e41d615e2dd775b66e4db33d182ab4f260
Adam Hollett <mail@adamhollett.com>
2017-10-02 20:27:58 -0400

    Add Windows setup

--- os/windows.sh
+++ os/windows.sh
@@ -1,1 +1,37 @@
 #!/bin/sh
+
+## Functions
+
+symlink_to_home() {
+  FILE=$1
+  # Only take action if the input file exists
+  if [ -f $FILE ]
+  then
+    # Symlink the file to the home directory
+    echo "Linking $(basename $FILE)"
+    ln -sf $FILE ~/$(basename $FILE)
+  fi
+}
+
+## Symlinks
+
+print_header 'create symlinks'
+
+### git
+
+symlink_to_home ~/dotfiles/git/.gitconfig
+symlink_to_home ~/dotfiles/git/.gitignore_global
+
+### shell
+
+symlink_to_home ~/dotfiles/shell/.inputrc
+
+#### bash
+
+symlink_to_home ~/dotfiles/shell/bash/.bash_profile
+symlink_to_home ~/dotfiles/shell/bash/.bashrc
+
+### ruby
+
+symlink_to_home ~/dotfiles/ruby/.ruby-version
+symlink_to_home ~/dotfiles/ruby/.rubocop.yml
