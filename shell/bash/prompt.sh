2cbf66dceca024714eb588c359c780f46ec9b78b
Adam Hollett <mail@adamhollett.com>
2018-08-13 15:21:32 -0400

    Update prompt code and colours

--- shell/bash/prompt.sh
+++ shell/bash/prompt.sh
@@ -4,18 +4,13 @@ PROMPT_DIRTRIM=2
 # Print the current git branch in the prompt, if we're in a git repository
 function git_branch() {
   BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
-  if [ ! "${BRANCH}" == "" ]
-  then
-    echo "${BRANCH} "
-  else
-    echo ""
-  fi
+  if [ ! "${BRANCH}" == "" ]; then echo "${BRANCH} "; fi
 }
 
 # A shortened path to the current directory
-CUR_PATH="\[\e[34m\]\w\[\e[m\]"
+CURRENT_PATH="\[\e[94m\]\w\[\e[m\]"
 
 # Some colourful chevrons
-CHEVRONS="\[\e[31m\]❯\[\e[m\]\[\e[33m\]❯\[\e[m\]\[\e[32m\]❯\[\e[m\]"
+CHEVRONS="\[\e[31m\]❯\[\e[35m\]❯\[\e[34m\]❯\[\e[m\]"
 
-export PS1="$CUR_PATH \[\e[32m\]\`git_branch\`\[\e[m\]$CHEVRONS "
+export PS1="$CURRENT_PATH \[\e[32m\]\`git_branch\`\[\e[m\]$CHEVRONS "
