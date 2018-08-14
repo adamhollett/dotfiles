e9e35b20d2b7bc42d579c71688b82cd351435195
Adam Hollett <mail@adamhollett.com>
2018-08-14 17:13:39 -0400

    Improve bash prompt

--- shell/bash/.bash_profile
+++ shell/bash/.bash_profile
@@ -5,8 +5,7 @@
 
 source ~/dotfiles/shell/path.sh
 source ~/dotfiles/shell/aliases.sh
-
-source ~/dotfiles/shell/bash/prompt.sh
+source ~/dotfiles/shell/bash/prompt.bash
 
 # Add dev if it's present
 if [[ -f /opt/dev/dev.sh ]] && [[ $- == *i* ]]; then source /opt/dev/dev.sh; fi
--- shell/bash/prompt.bash
+++ shell/bash/prompt.bash
@@ -2,9 +2,15 @@
 PROMPT_DIRTRIM=2
 
 # Print the current git branch in the prompt, if we're in a git repository
-function git_branch() {
-  BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
-  if [ ! "${BRANCH}" == "" ]; then echo "${BRANCH} "; fi
+function git_branch {
+  local branch=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+  if [[ ! $branch == "" ]]; then
+    if [[ $branch == "master" ]]; then
+      echo -e "\e[32mm\e[m "
+    else
+      echo -e "\e[32m$branch\e[m "
+    fi
+  fi
 }
 
 # A shortened path to the current directory
@@ -13,4 +19,4 @@ CURRENT_PATH="\[\e[94m\]\w\[\e[m\]"
 # Some colourful chevrons
 CHEVRONS="\[\e[31m\]❯\[\e[35m\]❯\[\e[34m\]❯\[\e[m\]"
 
-export PS1="$CURRENT_PATH \[\e[32m\]\`git_branch\`\[\e[m\]$CHEVRONS "
+export PS1="$CURRENT_PATH \`git_branch\`$CHEVRONS "
