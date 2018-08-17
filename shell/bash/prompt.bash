d540a2ba4d0cb56db1beea8741569e8c59cca4bd
Adam Hollett <mail@adamhollett.com>
2018-08-17 16:44:41 -0400

    Consolidate git info into one function

--- shell/bash/prompt.bash
+++ shell/bash/prompt.bash
@@ -1,22 +1,38 @@
-# Only show the last two directories of the current path
-PROMPT_DIRTRIM=2
-
-# Print the current git branch in the prompt, if we're in a git repository
-function git_branch {
-  local branch=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
-  if [[ ! $branch == "" ]]; then
-    if [[ $branch == "master" ]]; then
-      echo -e "\e[32mm\e[m "
+git_info () {
+  local branch=''
+  local status=''
+  
+  # Determine if we're in a git repo
+  local branchCheck=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+  if [[ ! $branchCheck == '' ]]; then
+    
+    # Determine the state of the branch
+    local statusCheck=`git status --porcelain 2> /dev/null`
+    if [[ ! $statusCheck == '' ]]; then
+      status='○'
     else
-      echo -e "\e[32m$branch\e[m "
+      status='●'
+      # status='◒'
     fi
+    
+    # Don't show the branch name if it's 'master'
+    if [[ $branchCheck == 'master' ]]; then
+      branch=''
+    else
+      branch="$branchCheck "
+    fi
+
+    echo -e "\e[32m$branch$status\e[m "
   fi
+  return
 }
 
+
 # A shortened path to the current directory
+PROMPT_DIRTRIM=2
 CURRENT_PATH="\[\e[94m\]\w\[\e[m\]"
 
 # Some colourful chevrons
 CHEVRONS="\[\e[31m\]❯\[\e[35m\]❯\[\e[34m\]❯\[\e[m\]"
 
-export PS1="$CURRENT_PATH \`git_branch\`$CHEVRONS "
+export PS1="$CURRENT_PATH \`git_info\`$CHEVRONS "
