7d36f5af242af1416ae03b5110c77d018099ba4a
Adam Hollett <mail@adamhollett.com>
2020-02-14 10:41:57 -0500

    Use winter colours

--- shell/zsh/prompt.zsh
+++ shell/zsh/prompt.zsh
@@ -2,7 +2,7 @@
 
 setopt prompt_subst
 
-CHEVRONS='%F{blue}❯%F{green}❯%F{yellow}❯%f'
+CHEVRONS='%F{063}❯%F{075}❯%F{087}❯%f'
 
 export PS1='$(fancy_dir) $(git_dot)$CHEVRONS '
 export RPROMPT='$(git_branch)'
