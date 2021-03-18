1670f8b953fe4890909d7b93b420b85872035ae6
Adam Hollett <mail@adamhollett.com>
2021-03-17 23:19:12 -0400

    Source chruby if it's installed

--- shell/common.sh
+++ shell/common.sh
@@ -3,4 +3,7 @@
 source ~/dotfiles/shell/functions.sh
 source ~/dotfiles/shell/aliases.sh
 source ~/dotfiles/shell/nvm.sh
+
 safe_source ~/dotfiles/shell/shopify.sh
+safe_source /usr/local/share/chruby/chruby.sh
+safe_source /usr/local/share/chruby/auto.sh
