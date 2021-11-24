4557ad1a8446ead320c244bc46e1549313d40fb0
Adam Hollett <mail@adamhollett.com>
2021-11-24 14:13:32 -0500

    Fix spacing and caps

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -4,25 +4,25 @@
 
 ### working copy (status)
 
-alias  gws='git status'
-alias  gwC='git clean -fd'
-alias  gwr='git reset --soft'
-alias  gwR='git reset --hard'
+alias gws='git status'
+alias gwC='git clean -fd'
+alias gwr='git reset --soft'
+alias gwR='git reset --hard'
 
 ### branch
 
-alias   gb='git branch'
-alias  gbc='git checkout -b'
-alias  gbd='git branch --delete'
-alias  gbD='git branch --delete --force'
-alias  gco='git checkout'
+alias  gb='git branch'
+alias gbc='git checkout -b'
+alias gbd='git branch --delete'
+alias gbD='git branch --delete --force'
+alias gco='git checkout'
 
 ### fetch
 
-alias   gf='git fetch'
-alias  gfa='git fetch --all'
-alias  gfc='git clone'
-alias  gfm='git pull'
+alias  gf='git fetch'
+alias gfa='git fetch --all'
+alias gfc='git clone'
+alias gfm='git pull'
 
 ### clean
 
@@ -30,8 +30,8 @@ alias gcfd='git clean -fd'
 
 ### index
 
-alias   ga='git add'
-alias  gaa='git add -A'
+alias  ga='git add'
+alias gaa='git add -A'
 
 ### commit
 
@@ -40,33 +40,29 @@ alias gcam='git commit --all --message'
 
 ### push
 
-alias   gp='git push'
-alias  gpc='git push --set-upstream origin `git rev-parse --abbrev-ref HEAD`'
-alias  gpo='git push origin'
+alias  gp='git push'
+alias gpc='git push --set-upstream origin `git rev-parse --abbrev-ref HEAD`'
+alias gpo='git push origin'
 
 ### stash
 
-alias   gs='git stash -u'
-alias  gss='git stash save -u'
-alias  gsl='git stash list'
-alias  gsp='git stash pop'
+alias  gs='git stash -u'
+alias gss='git stash save -u'
+alias gsl='git stash list'
+alias gsp='git stash pop'
 
+## Node
 
-## Node.js
-
-alias   nr='npm run'
-
+alias nr='npm run'
 
 ## Ruby
 
-alias   be='bundle exec'
+alias be='bundle exec'
 
-
-## Misc
+## misc
 
 alias ls='ls -G'
 
-
-## Fun
+## fun
 
 alias hack='open http://hackertyper.net'
