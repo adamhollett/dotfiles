40be6bdc535ca6fd7d541edc2af6c01775c95fd7
Adam Hollett <mail@adamhollett.com>
2021-11-29 11:56:22 -0500

    Move bash functions to their own files

--- shell/bash/prompt.bash
+++ shell/bash/prompt.bash
@@ -1,12 +1,7 @@
 #!/bin/false
-#
-# An enhanced, colourful bash prompt featuring git branch and status info.
 
-# A shortened path to the current directory
+# Truncate the path to the current directory
 PROMPT_DIRTRIM=2
 
-chevrons () {
-  echo "$(cecho ❯ light-blue)$(cecho ❯ light-green)$(cecho ❯ light-yellow)"
-}
-
-export PS1="$(cecho '\w' cyan) \`git_info\`$(chevrons) "
+PS1="$(cecho '\w' cyan) $(git_info)$(chevrons) "
+export PS1
--- shell/functions.sh
+++ shell/functions.sh
@@ -5,34 +5,3 @@
 safe_source () {
   if [ -f $1 ]; then source $1; fi
 }
-
-# Print a message in color.
-# https://bytefreaks.net/gnulinux/bash/cecho-a-function-to-print-using-different-colors-in-bash
-cecho () {
-  declare message=${1:-""}
-  declare   color=${2:-"default"}
-
-  declare -A colors
-  colors=(
-          [default]="\e[39m"
-            [black]="\e[30m"
-              [red]="\e[31m"
-            [green]="\e[32m"
-           [yellow]="\e[33m"
-             [blue]="\e[34m"
-          [magenta]="\e[35m"
-             [cyan]="\e[36m"
-             [gray]="\e[37m"
-        [light-red]="\e[91m"
-      [light-green]="\e[92m"
-     [light-yellow]="\e[93m"
-       [light-blue]="\e[94m"
-    [light-magenta]="\e[95m"
-       [light-cyan]="\e[96m"
-       [light-gray]="\e[97m"
-  )
-
-  color=${colors[$color]}
-
-  echo -e "\x01${color}\x02${message}\x01\e[m\x02"
-}
