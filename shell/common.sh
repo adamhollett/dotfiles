38f81d71a4596e9aca407ce3c436ba3d55ca74c8
Adam Hollett <adam.hollett@shopify.com>
2021-12-10 13:27:46 -0400

    Rename safe_source to sourceif, add homebrew

--- shell/common.sh
+++ shell/common.sh
@@ -1,10 +1,11 @@
 #!/bin/false
 # shellcheck shell=bash
 
-source ~/dotfiles/shell/functions.sh
-source ~/dotfiles/shell/aliases.sh
-source ~/dotfiles/shell/nvm.sh
+source   ~/dotfiles/shell/functions.sh
+source   ~/dotfiles/shell/aliases.sh
+source   ~/dotfiles/shell/nvm.sh
+sourceif ~/dotfiles/shell/shopify.sh
+sourceif /usr/local/share/chruby/chruby.sh
+sourceif /usr/local/share/chruby/auto.sh
 
-safe_source ~/dotfiles/shell/shopify.sh
-safe_source /usr/local/share/chruby/chruby.sh
-safe_source /usr/local/share/chruby/auto.sh
+[[ -x /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
--- shell/functions.sh
+++ shell/functions.sh
@@ -3,6 +3,4 @@
 
 # Source a file only if it exists.
 # Use for files that might not be on all machines (work-specific).
-safe_source () {
-  if [ -f "$1" ]; then source "$1"; fi
-}
+sourceif () { if [ -f "$1" ]; then source "$1"; fi }
