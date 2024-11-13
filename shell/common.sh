8105691c070e3e2e58e03bed4a19ada101d09c25
Adam Hollett <mail@adamhollett.com>
2024-11-12 21:36:43 -0400

    asdf -> mise

--- shell/common.sh
+++ shell/common.sh
@@ -4,18 +4,9 @@
 source   ~/dotfiles/shell/functions.sh
 source   ~/dotfiles/shell/aliases.sh
 sourceif ~/dotfiles/shell/shopify.sh
-sourceif /usr/local/opt/asdf/libexec/asdf.sh
 sourceif /usr/local/share/chruby/chruby.sh
 sourceif /usr/local/share/chruby/auto.sh
 
 ## homebrew
 
 [[ -x /opt/homebrew/bin/brew ]] && eval "$(/opt/homebrew/bin/brew shellenv)"
-
-## nvm
-
-if [[ $(which nvm) ]]; then
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-fi
